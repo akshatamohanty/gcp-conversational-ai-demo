a58806aa794f3173ee9612a34b9bdb00dc460598
Akshata Mohanty <akshatamohanty@gmail.com>
2020-05-26 17:39:38 +0800

    [Feature] Add basic yes/no

--- client/src/screens/intro/Intro.js
+++ client/src/screens/intro/Intro.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect, useCallback } from 'react'
 import './Intro.css'
 
+//
+import SpeechSingleton from '../../Voice'
+
 const Intro = ({ name, onStart, onEnd }) => {
+  const detectYes = useCallback((transcript) => {
+    if (transcript === 'yes') {
+      onStart()
+    }
+
+    if (transcript === 'no') {
+      onEnd()
+    }
+  }, [onStart, onEnd])
+
+  useEffect(() => {
+    const speech = new SpeechSingleton()
+    if (speech) {
+      speech.onSpeech(detectYes)
+    } else {
+      console.log('Web Speech Api not available')
+    }
+  }, [detectYes])
+
   return (
     <div className="app">
       <header className="app_header">
         Hello {name}!
       </header>
       <p>Would you like to play a game?</p>
-      <button onClick={onStart}>Yes</button>
-      <button onClick={onEnd}>No</button>
     </div>
   )
 }
